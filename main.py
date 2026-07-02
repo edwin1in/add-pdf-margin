0b4849f25fbdd8e8363c39dcc49d2696591dd442
edwin1in <edwinzhanminglin@gmail.com>
2026-07-02 12:38:43 -0700

    feat: add margin adjustment function

--- main.py
+++ main.py
@@ -2,23 +2,47 @@ from pdf2image import convert_from_path, convert_from_bytes
 from pdf2image.exceptions import (
     PDFInfoNotInstalledError,
     PDFPageCountError,
-    PDFSyntaxError
+    PDFSyntaxError,
 )
-
 from reportlab.pdfgen import canvas
 from reportlab.lib.utils import ImageReader
 
-images = convert_from_path("test.pdf")
 
-width = images[0].width
-height = images[0].height
+def change_margin(
+    pdfpath: str = "",
+    lmargin: int = 0,
+    rmargin: int = 0,
+    tmargin: int = 0,
+    bmargin: int = 0,
+) -> None:
+    try:
+        images = convert_from_path("test.pdf")
+    except FileNotFoundError:
+        raise FileNotFoundError("PDF not found")
 
-c = canvas.Canvas(".pdf", pagesize=(width+3000, height))
-c.drawImage(ImageReader(images[0]),0,0, width=width, height=height)
-c.save()
+    if len(images) < 1:
+        raise FileNotFoundError("PDF contains no pages.")
+
+    pdf_width = images[0].width
+    pdf_height = images[0].height
+
+    lmargin, rmargin, tmargin, bmargin = [
+        max(0, m) for m in (lmargin, rmargin, tmargin, bmargin)
+    ]
+
+    c = canvas.Canvas(
+        "type.pdf",
+        pagesize=(pdf_width + (lmargin + rmargin), pdf_height + (tmargin + bmargin)),
+    )
+    for img in images:
+        # (0,0) origin point at the lower left corner of the page
+        # Furthermore the first coordinate x goes to the right and the second coordinate y goes up, by default.
+        c.drawImage(
+            ImageReader(img), lmargin, bmargin, width=pdf_width, height=pdf_height
+        )
+        c.showPage()
+    c.save()
 
 
-
-
-
-
+if __name__ == "__main__":
+    pass
