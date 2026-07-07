054216bf210cca451ca20300b15bd4e1a95122e0
edwin1in <edwinzhanminglin@gmail.com>
2026-07-06 23:15:40 -0700

    feat: add margins, unit, change margin user inputs

--- gui.py
+++ gui.py
@@ -1,7 +1,16 @@
 from PyQt6.QtCore import QSize, Qt
-from PyQt6.QtWidgets import QApplication, QMainWindow, QPushButton, QLineEdit, QHBoxLayout, QWidget
+from PyQt6.QtWidgets import (
+    QApplication,
+    QMainWindow,
+    QPushButton,
+    QSpinBox,
+    QHBoxLayout,
+    QVBoxLayout,
+    QWidget,
+    QComboBox,
+    QLabel,
+)
 
-# TODO: check out QStackedLayout https://www.pythonguis.com/tutorials/pyqt6-layouts/
 
 class Window(QMainWindow):
     def __init__(self):
@@ -9,7 +18,27 @@ class Window(QMainWindow):
 
         self.setWindowTitle("app")
 
-        button = QPushButton("Press Me!")
+        app_layout = QVBoxLayout()
+        input_layout = QHBoxLayout()
 
-        # Set the central widget of the Window.
-        self.setCentralWidget(button)
+        labels = ["Left margin", "Right margin", "Top margin", "Bottom margin"]
+        margins = [QSpinBox() for _ in range(0, 4)]
+
+        self.lmargin, self.rmargin, self.tmargin, self.bmargin = margins
+
+        for label, margin in zip(labels, margins):
+            input_layout.addWidget(QLabel(label))
+            input_layout.addWidget(margin)
+        app_layout.addLayout(input_layout)
+        units = ["px", "in", "cm", "mm"]
+        self.unit = QComboBox()
+        self.unit.addItems(units)
+        input_layout.addWidget(QLabel("Units"))
+        input_layout.addWidget(self.unit)
+
+        self.button = QPushButton("Add margin(s)")
+        app_layout.addWidget(self.button)
+
+        widget = QWidget()
+        widget.setLayout(app_layout)
+        self.setCentralWidget(widget)
